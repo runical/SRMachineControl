f27bb1264fed1bb424d03fb90ccc032b6812a08b
Peter van den Hurk <runical1991@gmail.com>
2014-06-24 16:31:47 -0400

    Removed all of the debugging statements and removed the testing specific code. This to make merging into the main tree easier.

--- SRM.cpp
+++ SRM.cpp
@@ -22,9 +22,7 @@
   THE SOFTWARE.
 */
 
-#define DEBUG 1
-
-//#include "arduino.h"
+#include "arduino.h"
 #include <stdio.h>
 #include "SRM.h"
 
@@ -36,70 +34,30 @@ PhysicalSwitch::PhysicalSwitch(int pin)
 {
 	// PhysicalSwitch sets the pin mode to output, inits it to low and saves the pin number.
 	
-	// pinMode(pin, OUTPUT);
-	// digitalWrite(pin, LOW);
+	pinMode(pin, OUTPUT);
+	digitalWrite(pin, LOW);
 	this->_state = false;
 	this->_pin = pin;
-	
-	#ifdef DEBUG
-		printf("Switch initialized on pin %i \n", _pin);
-	#endif
 };
 
 void PhysicalSwitch::Activate()
-{
-	#ifdef DEBUG
-	
-	printf("Switch on pin %i has state %i\n", this->_pin, this->_state);
-	
-	if(this->_state)
-	{
-		printf("Switch on pin %i already activated (state %i)\n", _pin, _state);
-	};
-	#endif
-	
+{	
 	// Activate a switch
 	if(not this->_state)
 	{
-		// digitalWrite(_pin, HIGH);
+		digitalWrite(_pin, HIGH);
 		this->_state = true;
-		
-		#ifdef DEBUG
-			printf("Activated switch on pin %i (state %i)\n", _pin, _state);
-		#endif
 	};
-	
-	#ifdef DEBUG
-		printf("Switch on pin %i has state %i\n", this->_pin, this->_state);
-	#endif
 };
 
 void PhysicalSwitch::Deactivate()
 {
-	#ifdef DEBUG
-	printf("Switch on pin %i has state %i\n", this->_pin, this->_state);
-	
-	if(not this->_state)
-	{
-		printf("Switch on pin %i already deactivated (state %i)\n", _pin, _state);
-	};
-	#endif
-	
 	// Deactivate a switch
 	if(this->_state)
 	{
-		// digitalWrite(_pin, LOW);
+		digitalWrite(_pin, LOW);
 		this->_state = false;
-		
-		#ifdef DEBUG
-			printf("Deactivated switch on pin %i (state %i)\n", _pin, _state);
-		#endif
 	};
-	
-	#ifdef DEBUG
-		printf("Switch on pin %i has state %i\n", this->_pin, this->_state);
-	#endif
-	
 };
 
 //////////////////////////////////////////
@@ -113,40 +71,24 @@ SwitchState::SwitchState(PhysicalSwitch** activeSwitches, int nSwitches)
 	this->_nSwitches = nSwitches;
 	this->_next = this;
 	this->_previous = this;
-	
-	#ifdef DEBUG
-		printf("SwitchState initialized with %i switches\n", _nSwitches);
-	#endif
 };
 
 void SwitchState::InsertSwitchState(SwitchState* insertedState)
 {
-	#ifdef DEBUG
-		printf("Start adding switchstate\n");
-	#endif
 	insertedState->SetPrevious(this);
 	insertedState->SetNext(this->_next);
 	_next->SetPrevious(insertedState);
 	this->SetNext(insertedState);
-	#ifdef DEBUG
-		printf("Added SwitchState\n");
-	#endif
 };
 
 void SwitchState::SetNext(SwitchState* insertedState)
 {
 	_next = insertedState;
-	#ifdef DEBUG
-		printf("Set next\n");
-	#endif
 };
 
 void SwitchState::SetPrevious(SwitchState* insertedState)
 {
 	_previous = insertedState;
-	#ifdef DEBUG
-		printf("Set previous\n");
-	#endif
 };
 
 SwitchState* SwitchState::GetNext()
@@ -177,36 +119,20 @@ Bridge::Bridge(int numberOfSwitches, PhysicalSwitch** switches)
 {
 	_nSwitches = numberOfSwitches;
 	_switches = switches;
-	
-	#ifdef DEBUG
-		printf("Bridge initialized with %i switches\n", _nSwitches);
-	#endif
 };
 
 void Bridge::TurnOff()
 {
-	#ifdef DEBUG
-		printf("Starting with deactivation of switches.\n");
-	#endif
-	
 	int i;
 	
 	for(i = 0 ; i < _nSwitches ; i++)
 	{
 		_switches[i]->Deactivate();
 	};
-	
-	#ifdef DEBUG
-		printf("Switches deactivated.\n");
-	#endif
 };
 
 void Bridge::ActivateState(SwitchState* activatedState)
 {
-	#ifdef DEBUG
-		printf("Started with activation of a new state.\n");
-	#endif
-	
 	int limit = activatedState->GetNumberOfSwitches();
 	PhysicalSwitch** tempSwitches = activatedState->GetSwitches();
 	this->TurnOff();
@@ -217,27 +143,21 @@ void Bridge::ActivateState(SwitchState* activatedState)
 	{
 		tempSwitches[i]->Activate();
 	};
-	
-	#ifdef DEBUG
-		printf("Activated the new state\n");
-	#endif
 };
 
 //////////////////////////////////////////
 // 				Controller				//
 //////////////////////////////////////////
 
-// Needs to be implemented, Work in progress
+// The controller implements the logic
 
 Controller::Controller(SwitchState* topState, Bridge* theBridge, Encoder* theEncoder)
 {
+	// Init with the top state.
 	this->_startState = topState;
 	this->_currentState = topState;
 	this->_bridge = theBridge;
 	this->_encoder = theEncoder;
-	#ifdef DEBUG
-		printf("Controller initialized\n");
-	#endif
 };
 
 void Controller::ActivateNextState()
@@ -254,16 +174,19 @@ void Controller::ActivatePreviousState()
 
 void Controller::CalculateOffset()
 {
+	// Calculation of the offset (rethink how this is integrated with the system)
 	return;
 };
 
 void Controller::CalculateTransitions(int numberOfElectricRevPerMechRev, int nStates)
 {
+	// Calculation of the transition points
 	return;
 };
 
 void Controller::Logic()
 {
+	// The controller logic. Needs to be implemented/thought out.
 	this->ActivatePreviousState();
 	return;
 };
--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-g++ LibraryTest.cpp SRM.cpp Encoder.cpp -o Test.o
-
-chmod +x Test.o
-
-./Test.o
